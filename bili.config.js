5bf7158fdc068b98af6f5ebd0e71600a2e54d1bc
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-14 11:55:52 +0100

    Move server files into dist/server

--- bili.config.js
+++ bili.config.js
@@ -27,7 +27,7 @@ module.exports = {
       entries: [{ find: '@', replacement: path.join(__dirname, './src') }]
     },
     copy: {
-      targets: [{ src: 'src/server/**', dest: 'dist' }]
+      targets: [{ src: 'src/server/**', dest: 'dist/server' }]
     }
   },
   resolvePlugins: {
