5cbe7dbf783b9a308339297270aa1b80d9ea7f29
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-14 11:47:43 +0100

    Change server files location in the dist folder

--- bili.config.js
+++ bili.config.js
@@ -27,7 +27,7 @@ module.exports = {
       entries: [{ find: '@', replacement: path.join(__dirname, './src') }]
     },
     copy: {
-      targets: [{ src: 'src/server/**', dest: 'dist/dist' }]
+      targets: [{ src: 'src/server/**', dest: 'dist' }]
     }
   },
   resolvePlugins: {
