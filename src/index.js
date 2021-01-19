cf5d36fb878426e6e93125dfe9b2255ef345e575
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-19 10:09:47 +0100

    Revert help comments

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const initState = () => ({ fileName: null, videoId: null, playable: false });
 
 export { Edit, Toolbar };
 
+/**
+ * The fields that need to be customized are:
+ * name: a string that is displayed to a user in the editor
+ * ui->icon: a string representing the name of the MDI (https://materialdesignicons.com/)
+ * icon that is displayed to the user in the editor
+ * ui->forceFullWidth: a boolean value which defines if the element can only be
+ * added as full width element
+ */
 export default {
   ...info,
   initState,
