d341840b238bf66323dfa6a9a4189e7abbdf35dd
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-19 13:59:11 +0100

    Implement element status tracker

--- src/edit/index.vue
+++ src/edit/index.vue
@@ -9,13 +9,13 @@
       active-placeholder="Use toolbar to upload the video"
       active-icon="mdi-arrow-up" />
     <div v-else>
-      <div v-if="error" class="overlay">
+      <div v-if="showError" class="overlay">
         <div class="message error--text">
           <v-icon class="error--text">mdi-alert</v-icon>
-          {{ error.message || 'Error loading media!' }}
+          {{ error || 'Error loading media!' }}
         </div>
       </div>
-      <div v-if="infoMessage" class="overlay">
+      <div v-if="!showError && infoMessage" class="overlay">
         <div class="message info--text">
           <v-progress-circular indeterminate color="info" class="mr-4" />
           {{ infoMessage }}
@@ -27,14 +27,15 @@
 </template>
 
 <script>
-import axios from 'axios';
+import { ELEMENT_STATE } from '../shared';
 import ElementPlaceholder from '../tce-core/ElementPlaceholder.vue';
 import get from 'lodash/get';
+import upload from '../createUpload';
 
 const DEFAULT_ERROR_MSG = 'Something went wrong.';
+const CANCEL_UPLOAD_ERROR_MSG = 'Upload canceled by leaving the page.';
 const UPLOADING_MSG = 'Video is uploading... Do not leave the page.';
 const PROCESSING_MSG = 'Video is processing...';
-const CHUNK_SIZE = 64 * 1024 * 1024;
 
 export default {
   name: 'tce-api-video',
@@ -44,77 +45,68 @@ export default {
     isFocused: { type: Boolean, default: false },
     isDisabled: { type: Boolean, default: false }
   },
-  data: () => ({ error: null, file: null, loading: false }),
+  data: () => ({ file: null }),
   computed: {
-    videoId: ({ element }) => get(element, 'data.videoId', ''),
-    uploadUrl: ({ element }) => get(element, 'data.uploadUrl', null),
-    embedCode: ({ element }) => get(element, 'data.embedCode', ''),
-    fileName: ({ element }) => get(element, 'data.fileName', ''),
-    playable: ({ element }) => get(element, 'data.playable', false),
+    videoId: ({ element }) => element.data?.videoId,
+    uploadUrl: ({ element }) => element.data?.uploadUrl,
+    embedCode: ({ element }) => element.data?.embedCode,
+    fileName: ({ element }) => element.data?.fileName,
+    playable: ({ element }) => element.data?.playable,
+    status: ({ element }) => element.data?.status,
+    error: ({ element }) => element.data?.error,
+    showError: ({ status }) => status === ELEMENT_STATE.ERROR,
     showPlaceholder: ({ error, fileName }) => !error && !fileName,
-    infoMessage: ({ error, loading, playable }) => {
+    infoMessage: ({ error, status, playable }) => {
       if (error) return;
-      if (loading) return UPLOADING_MSG;
+      if (status === ELEMENT_STATE.UPLOADING) return UPLOADING_MSG;
       if (!playable) return PROCESSING_MSG;
     }
   },
   methods: {
-    upload({ url, file, videoId }) {
-      if (CHUNK_SIZE > file.size) return this.post({ url, videoId, chunk: file });
-      const chunks = [];
-      const { size } = file;
-      for (let offset = 0; offset < size; offset += CHUNK_SIZE) {
-        const end = Math.min(offset + CHUNK_SIZE, size);
-        const chunk = file.slice(offset, end);
-        chunks.push({ chunk, offset, end });
-      }
-      return Promise.all(chunks.map(it => this.post({ url, videoId, size, ...it })));
-    },
-    post({ url, videoId, chunk, size, offset, end }) {
-      const headers = {
-        'Content-Type': 'multipart/form-data'
-      };
-      if (offset !== undefined && end !== undefined) {
-        headers['Content-Range'] = `bytes ${offset}-${end - 1}/${size}`;
-      }
-      const formData = new FormData();
-      formData.append('file', chunk);
-      formData.append('videoId', videoId);
-      return axios.post(url, formData, { headers });
-    },
     appendVideo() {
       const { player } = this.$refs;
       if (!player) return;
       player.innerHTML = this.embedCode;
+    },
+    checkIfError() {
+      const { status, file, element } = this;
+      if (status !== ELEMENT_STATE.UPLOADING || file) return;
+      this.$emit('save', {
+        ...element.data,
+        status: ELEMENT_STATE.ERROR,
+        error: CANCEL_UPLOAD_ERROR_MSG
+      });
     }
   },
   watch: {
-    embedCode() {
-      this.appendVideo();
-    },
+    embedCode: 'appendVideo',
     videoId() {
       const { videoId, file, uploadUrl: url } = this;
       if (!videoId || !file || !url) return;
-      return this.upload({ url, file, videoId })
-        .then(() => { this.file = null; })
-        .catch(err => {
-          const message = get(err, 'response.data.title', DEFAULT_ERROR_MSG);
-          this.$elementBus.emit('error', { error: { message } });
+      return upload({ url, file, videoId })
+        .then(() => {
+          this.file = null;
+          this.$emit('save', { ...this.element.data, status: ELEMENT_STATE.UPLOADED });
         })
-        .finally(() => { this.loading = false; });
+        .catch(err => this.$elementBus.emit('error', err.response));
     }
   },
   mounted() {
+    this.checkIfError();
     this.appendVideo();
 
     this.$elementBus.on('save', ({ file }) => {
-      this.error = null;
       this.file = file;
-      this.loading = true;
-      this.$emit('save', { fileName: file.name });
+      this.$emit('save', { fileName: file.name, status: ELEMENT_STATE.UPLOADING });
     });
 
-    this.$elementBus.on('error', ({ error }) => { this.error = error; });
+    this.$elementBus.on('error', ({ data }) => {
+      this.$emit('save', {
+        ...this.element.data,
+        status: ELEMENT_STATE.ERROR,
+        error: get(data, 'error.message', DEFAULT_ERROR_MSG)
+      });
+    });
   },
   components: { ElementPlaceholder }
 };
@@ -142,23 +134,5 @@ export default {
 .player {
   height: 25.625rem;
   background: #000;
-
-  ::v-deep {
-    > div, .plyr--video, .plyr__video-wrapper, video {
-      height: 100%;
-    }
-
-    .plyr.plyr--vimeo:fullscreen {
-      padding-bottom: 56.25%;
-    }
-
-    .plyr__video-wrapper {
-      padding-bottom: 0 !important;
-    }
-
-    .plyr__control--overlaid {
-      display: none;
-    }
-  }
 }
 </style>
--- src/server/index.js
+++ src/server/index.js
@@ -1,54 +1,63 @@
 'use strict';
 
-const { ClientSandbox } = require('./apiVideo');
-const info = require('../info');
+const { createSandBoxClient } = require('./apiVideo');
+const { ELEMENT_STATE } = require('../shared');
 
-// process.env approach should be replaced with a more efficient way of accessing env variables!
-const client = new ClientSandbox({ apiKey: process.env.API_VIDEO_API_KEY });
-
-function beforeSave(asset) {
-  const { videoId, fileName } = asset.data;
-  if (videoId || !fileName) return asset;
+function beforeSave(asset, { config: { tceApiVideo } }) {
+  const { videoId, fileName, status } = asset.data;
+  if (isError(status) || videoId || !fileName) return asset;
+  const client = createSandBoxClient({ apiKey: tceApiVideo.apiKey });
   return client.videos.create(fileName)
     .then(({ videoId }) => {
       asset.data.playable = false;
       asset.data.videoId = videoId;
       return asset;
+    })
+    .catch(error => {
+      asset.data.status = ELEMENT_STATE.ERROR;
+      asset.data.error = error.message;
+      return asset;
     });
 }
 
-async function afterSave(asset) {
-  const { videoId, playable } = asset.data;
-  if (!videoId || playable) return asset;
-  const interval = setInterval(async () => {
-    const {
-      ingest: { status },
-      encoding: { playable }
-    } = await client.videos.status(videoId);
-    if (status === 'uploaded' && playable) {
-      clearInterval(interval);
-      delete asset.data.uploadUrl;
-      await asset.update({ data: { ...asset.data, playable: true } });
-    }
-  }, 5000);
+async function afterSave(asset, { config: { tceApiVideo } }) {
+  const { videoId, playable, status } = asset.data;
+  if (isError(status) || !videoId || playable) return asset;
+  const client = createSandBoxClient({ apiKey: tceApiVideo.apiKey });
+  if (status === ELEMENT_STATE.UPLOADED) {
+    const interval = setInterval(async () => {
+      const {
+        ingest: { status },
+        encoding: { playable }
+      } = await client.videos.status(videoId);
+      if (status === 'uploaded' && playable) {
+        clearInterval(interval);
+        delete asset.data.uploadUrl;
+        await asset.update({ data: { ...asset.data, playable: true } });
+      }
+    }, 5000);
+  }
   asset.data.uploadUrl = await client.videos.getUploadUrl();
   return asset;
 }
 
-function afterLoaded(asset) {
-  const { videoId, playable } = asset.data;
-  if (!videoId || !playable) return asset;
+function afterLoaded(asset, { config: { tceApiVideo } }) {
+  const { videoId, playable, status } = asset.data;
+  if (isError(status) || !videoId || !playable) return asset;
+  const client = createSandBoxClient({ apiKey: tceApiVideo.apiKey });
   return client.videos.get(videoId)
     .then(res => {
       asset.data.embedCode = res.assets.iframe;
       asset.data.url = res.assets.mp4;
       return asset;
+    })
+    .catch(error => {
+      asset.data.status = ELEMENT_STATE.ERROR;
+      asset.data.error = error.message;
+      return asset;
     });
 }
 
-module.exports = {
-  ...info,
-  beforeSave,
-  afterSave,
-  afterLoaded
-};
+const isError = state => state === ELEMENT_STATE.ERROR;
+
+module.exports = { beforeSave, afterSave, afterLoaded };
