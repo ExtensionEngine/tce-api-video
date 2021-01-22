ff27ad8b6b07b11ca6b7cfe1643e34e12648b716
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-22 09:25:51 +0100

    Move interval status checking to its own function

--- src/server/index.js
+++ src/server/index.js
@@ -13,32 +13,30 @@ function beforeSave(asset, { config: { tce } }) {
       asset.data.videoId = videoId;
       return asset;
     })
-    .catch(error => {
-      asset.data.status = ELEMENT_STATE.ERROR;
-      asset.data.error = error.message;
-      return asset;
-    });
+    .catch(error => handleError(error, asset));
 }
 
 async function afterSave(asset, { config: { tce } }) {
   const { videoId, playable, status } = asset.data;
   if (isError(status) || !videoId || playable) return asset;
   const client = createSandBoxClient({ apiKey: tce.apiVideoApiKey });
-  if (status === ELEMENT_STATE.UPLOADED) {
-    const interval = setInterval(async () => {
-      const {
-        ingest: { status },
-        encoding: { playable }
-      } = await client.videos.status(videoId);
-      if (status === 'uploaded' && playable) {
-        clearInterval(interval);
-        delete asset.data.uploadUrl;
-        await asset.update({ data: { ...asset.data, playable: true } });
-      }
-    }, 5000);
-  }
+  if (status === ELEMENT_STATE.UPLOADED) trackPlayableStatus(asset, client);
   asset.data.uploadUrl = await client.videos.getUploadUrl();
   return asset;
+}
+
+async function trackPlayableStatus(asset, client) {
+  const interval = setInterval(async () => {
+    const {
+      ingest: { status },
+      encoding: { playable }
+    } = await client.videos.status(videoId);
+    if (status === 'uploaded' && playable) {
+      clearInterval(interval);
+      delete asset.data.uploadUrl;
+      await asset.update({ data: { ...asset.data, playable: true } });
+    }
+  }, 5000);
 }
 
 function afterLoaded(asset, { config: { tce } }) {
@@ -51,13 +49,15 @@ function afterLoaded(asset, { config: { tce } }) {
       asset.data.url = res.assets.mp4;
       return asset;
     })
-    .catch(error => {
-      asset.data.status = ELEMENT_STATE.ERROR;
-      asset.data.error = error.message;
-      return asset;
-    });
+    .catch(error => handleError(error, asset));
 }
 
 const isError = state => state === ELEMENT_STATE.ERROR;
+
+function handleError(error, asset) {
+  asset.data.status = ELEMENT_STATE.ERROR;
+  asset.data.error = error.message;
+  return asset;
+}
 
 module.exports = { beforeSave, afterSave, afterLoaded };
