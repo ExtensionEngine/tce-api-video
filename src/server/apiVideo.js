46cb336838de33b6906fd23d46f3c3452b242d01
ikovac <ikovacevic96k@gmail.com>
2021-02-19 10:14:03 +0100

    Pass token ttl time as an argument 👽

--- src/server/apiVideo.js
+++ src/server/apiVideo.js
@@ -2,6 +2,8 @@
 
 const apiVideo = require('@api.video/nodejs-sdk');
 
+const TTL = 300; // Token time to live: 300s = 5min
+
 function createClient({ apiKey, isSandBox }) {
   return isSandBox
     ? new apiVideo.ClientSandbox({ apiKey })
@@ -10,7 +12,7 @@ function createClient({ apiKey, isSandBox }) {
 
 async function getUploadUrl(client) {
   const baseUrl = client.baseUri;
-  const token = await client.tokens.generate();
+  const token = await client.tokens.generate(TTL);
   const url = new URL('upload', baseUrl);
   url.searchParams.set('token', token);
   return url.href;
